afb09bfb534a9b6cea971702ea811a738768ac20
Slazakpawel <slazak.p2@gmail.com>
2025-12-03 14:49:04 +0100

    zbieranie danych przez W0

--- src/W0/main.cpp
+++ src/W0/main.cpp
@@ -2,75 +2,144 @@
 #include <Wire.h>
 #include "morslib.h"
 
-#define NODE_ID             0xFF   // ID tego węzła
-#define MAX_ZONE_2_NODES    10     // Maks. liczba węzłów I2C
-#define MAX_DATA_SIZE       32     // Maks. rozmiar danych z jednego urządzenia
-#define MAX_REGS_PER_SENSOR 8      // Maks. liczba rejestrów na sensor
-#define DATA_PULL_FREQ      10000  // Częst. odpytywania o dane
+#define NODE_ID 0xFF          // ID tego węzła
+#define MAX_ZONE_2_NODES 10   // Maks. liczba węzłów I2C
+#define MAX_DATA_SIZE 32      // Maks. rozmiar danych z jednego urządzenia
+#define MAX_REGS_PER_SENSOR 8 // Maks. liczba rejestrów na sensor
+#define DATA_PULL_FREQ 10000  // Częst. odpytywania o dane
+#define MAX_SENSORS 16        // Maks. liczba czujników w sieci
+
+#define liczbaBajtowDoPobrania = MAX_SENSORS * MAX_REGS_PER_SENSOR * 2;
 
 morslib mymors(LED_BUILTIN, 200);
 
-struct SensorInfo {
+struct SensorInfo
+{
+  //uint8_t address;
+  uint8_t registers[MAX_REGS_PER_SENSOR];
+  uint8_t data[MAX_REGS_PER_SENSOR];
+};
+
+struct nodeInfo
+{
   uint8_t address;
 };
 
-SensorInfo sensors[MAX_ZONE_2_NODES];
-uint8_t sensorCount = 0;
+nodeInfo nodes[MAX_ZONE_2_NODES];
+uint8_t nodeCount = 0;
+
+SensorInfo sensors[MAX_SENSORS];
 
 unsigned long lastRequestTime = 0;
 
-// Faza wykrywania urządzeń I2C
-void discoverI2CDevices() {
-  Serial.println("Skanowanie magistrali I2C...");
-  sensorCount = 0;
+void addNode(uint8_t addr)
+{
+  if (nodeCount < MAX_ZONE_2_NODES)
+  {
+    nodes[nodeCount].address = addr;
+    nodeCount++;
+  }
+  else
+  {
+    Serial.println("Brak miejsca na kolejne czujniki!");
+  }
+}
 
-  for (uint8_t addr = 1; addr < 127; addr++) {
-    //mymors.handle();
+// Faza wykrywania urządzeń I2C
+void discoverI2CDevices()
+{
+  Serial.println("Skanowanie magistrali I2C...");
+  nodeCount = 0;
+
+  for (uint8_t addr = 1; addr < 127; addr++)
+  {
+    // mymors.handle();
     if (addr == 0x60 || addr == 0x6B)
       continue;
-    
+
     Wire.beginTransmission(addr);
     uint8_t error = Wire.endTransmission();
 
-    if (error == 0) {
-      if (sensorCount < MAX_ZONE_2_NODES) {
-        sensors[sensorCount].address = addr;
-        Serial.println(addr, HEX);
-        sensorCount++;
-      }
+    if (error == 0)
+    {
+
+      addNode(addr);
+      Serial.print("0x");
+      Serial.println(addr, HEX);
+      // if (sensorCount < MAX_ZONE_2_NODES) {
+      //   sensors[sensorCount].address = addr;
+      //   sensorCount++;
+      // }
     }
-    //delay(10);
   }
 
   Serial.print("Znaleziono ");
-  Serial.print(sensorCount);
-  Serial.println(" węzłów sąsiednich");  
+  Serial.print(nodeCount);
+  Serial.println(" węzłów sąsiednich");
 }
 
-void setup() {
+void zapytajODane(uint8_t addr)
+{
+  uint8_t odp = 0;
+  Wire.requestFrom(addr, 1, false);
+  while (Wire.available())
+    odp = Wire.read();
+  if (odp != 0 && odp != 255)
+  {
+    for (int j = 0; j < MAX_SENSORS;j++)
+      for (int i = 0; i < MAX_REGS_PER_SENSOR; i++)
+      {
+        Wire.requestFrom(addr, 2, false);
+        sensors[j].registers[i]=Wire.read();
+        sensors[j].data[i]=Wire.read();
+    }
+  }
+}
+
+void printDane(){
+  Serial.print("reg");
+      Serial.print("\t");
+      Serial.println("data");
+  for (int i = 0; i < MAX_SENSORS;i++){
+    Serial.println("--------------");
+    Serial.print("sensor ");
+    Serial.println(i);
+    for (int j = 0; j < MAX_REGS_PER_SENSOR;j++){
+      Serial.print(sensors[i].registers[j]);
+      Serial.print("\t");
+      Serial.println(sensors[i].registers[j]);
+    }
+  }
+}
+
+void setup()
+{
+  lastRequestTime = millis();
   mymors.begin();
-  Serial.begin(9600);    
+  Serial.begin(9600);
   Wire.begin();
-  while(!Serial);
+  while (!Serial)
+    ;
 
   Serial.println("W0 uruchomiony");
   mymors.queue('s');
-  mymors.queue('t');
-  mymors.queue('e');
-  delay(1000);
-  lastRequestTime = millis();
 }
 
-void loop() {
+void loop()
+{
   mymors.handle();
-  
-  if (millis() - lastRequestTime >= DATA_PULL_FREQ) {
+
+  if (millis() - lastRequestTime >= DATA_PULL_FREQ)
+  {
     discoverI2CDevices();
     lastRequestTime = millis();
-    mymors.queue('?');
-    mymors.queue('n');
+    mymors.queue('n', 1);
+    // for (int i = 0; i < nodeCount; i++)
+    // {
+    //   Serial.println(nodes[i].address, HEX);
+    // }
 
+    zapytajODane(nodes[1].address);
+    printDane();
   }
-
-  //delay(10);
 }
