8e022c76528ff8090f68f98b2a6f8d5e3c8201ea
deyanix <deyanix@deyanix.eu>
2025-12-03 19:13:58 +0100

    Poprawka funkcji bb_print_frame

--- lib/BroadcastBus/BroadcastConsts.h
+++ lib/BroadcastBus/BroadcastConsts.h
@@ -1,6 +1,8 @@
 #ifndef BROADCAST_CONSTS
 #define BROADCAST_CONSTS
 
+#include <Arduino.h>
+
 #define BB_MAX_REGISTERS (8)
 #define BB_MAX_SENSORS (8)
 
@@ -39,27 +41,6 @@ typedef struct __attribute__((packed)) {
     uint8_t crc;
 } bb_buffer;
 
-
-void bb_print_frame(bb_sensor_frame& frame) {
-    Serial.print("\tNode address = 0x");
-    Serial.println(frame.node_addr, HEX);
-    Serial.print("\tSensor address = 0x");
-    Serial.println(frame.sensor_addr, HEX);
-    Serial.print("\tSeq = ");
-    Serial.println(frame.seq);
-    Serial.print("\tFlags = 0b");
-    Serial.println(frame.flags, BIN);
-    Serial.print("\tRegisters length =");
-    Serial.println(frame.regs_len, DEC);
-    for (size_t i = 0; i < BB_MAX_REGISTERS; i++) {
-        Serial.print("\tRegister[");
-        Serial.print(i, DEC);
-        Serial.print("] = 0x");
-        Serial.print(frame.regs[i].addr, HEX);
-        Serial.print(" - ");
-        Serial.println(frame.regs[i].data);
-    }
-
-}
+void bb_print_frame(bb_sensor_frame& frame);
 
 #endif // BROADCAST_CONSTS
