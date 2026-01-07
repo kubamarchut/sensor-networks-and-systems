64ad7d466abc92edd0766e7b6cc8eeaac246bb5d
Kuba Marchut <kuba.marchut@gmail.com>
2026-01-07 11:39:14 +0100

    ping pong demo

--- src/W0/main.cpp
+++ src/W0/main.cpp
@@ -5,205 +5,15 @@
 #include "morslib.h"
 #include "wiring_private.h"
 
-#define MAX_ZONE_2_NODES 10   // Maks. liczba węzłów I2C
-#define MAX_REGS_PER_SENSOR 8 // Maks. liczba rejestrów na sensor
-#define DATA_PULL_FREQ 10000  // Częst. odpytywania o dane
-#define MAX_REGS 16*8        // Maks. liczba czujników w sieci
+struct PingPacket {
+  uint8_t from;
+  uint8_t to;
+  uint8_t type; // 1 = PING, 2 = PONG
+};
 
-#define I2C_STATUS_EMPTY    0x51
-#define I2C_STATUS_DATA     0x62
-#define I2C_STATUS_FINISHED 0x73
-//#define I2C_DEBUG 1
-
-#define WIRE1_SDA 0
-#define WIRE1_SCL 1
-#define WIRES_LEN 2
-
-TwoWire Wire1(&sercom3, WIRE1_SDA, WIRE1_SCL);
-TwoWire *Wires[2] = {&Wire, &Wire1};
+#define DEST_ID 2
 
 morslib mymors(LED_BUILTIN, 200);
-
-typedef struct __attribute__((packed)) {
-    uint8_t node_count;
-    uint8_t node_addresses[MAX_ZONE_2_NODES];
-    bb_sensor_frame sensor;
-} bb_node_frame;
-
-bb_node_frame frames[MAX_REGS];
-size_t frames_length;
-bool printed_frames = false;
-Stopwatch request_stopwatch = Stopwatch(DATA_PULL_FREQ);
-Stopwatch data_stopwatch = Stopwatch(500);
-
-struct nodeInfo {
-    size_t wireIdx;
-    uint8_t address;
-    bool finished;
-};
-nodeInfo nodes[MAX_ZONE_2_NODES];
-uint8_t nodeCount = 0;
-uint8_t seq = 0;
-
-void print_frames() {
-    if (printed_frames)
-        return;
-
-    for (size_t i = 0; i < frames_length; i++) {
-        Serial.print("Node2=");
-        for (size_t n = 0; n < frames[i].node_count; n++) {
-            Serial.print("0x");
-            Serial.print(frames[i].node_addresses[n], HEX);
-            Serial.print(" ");
-        }
-        bb_print_frame_compact(frames[i].sensor);
-    }
-    printed_frames = true;
-    frames_length = 0;
-}
-
-void addNode(size_t wireIdx, uint8_t addr) {
-    if (nodeCount < MAX_ZONE_2_NODES) {
-        Serial.print("Znaleziono węzeł (seq=");
-        Serial.print(seq);
-        Serial.print(") dla adresu ");
-        Serial.print("0x");
-        Serial.println(addr, HEX);
-        nodes[nodeCount].wireIdx = wireIdx;
-        nodes[nodeCount].address = addr;
-        nodes[nodeCount].finished = false;
-        nodeCount++;
-    } else {
-        Serial.println("Brak miejsca na kolejne czujniki!");
-    }
-}
-
-// Faza wykrywania urządzeń I2C
-void discoverI2CDevices() {
-    Serial.println("Skanowanie magistrali I2C...");
-    nodeCount = 0;
-    seq++;
-
-    for (size_t wireIdx = 0; wireIdx < WIRES_LEN; wireIdx++) {
-        TwoWire *wire = Wires[wireIdx];
-        for (uint8_t addr = 1; addr < 127; addr++) {
-            if (addr == 0x60 || addr == 0x6B)
-                continue;
-
-            wire->beginTransmission(addr);
-            wire->write('A');
-            wire->write(seq);
-            uint8_t error = wire->endTransmission();
-
-            if (error == 0) {
-                addNode(wireIdx, addr);
-            }
-        }
-    }
-
-    Serial.print("Znaleziono ");
-    Serial.print(nodeCount);
-    Serial.println(" węzłów sąsiednich");
-}
-
-void requestData(uint8_t i) {
-    Crc8 crc;
-    uint8_t addr = nodes[i].address;
-    TwoWire *wire = Wires[nodes[i].wireIdx];
-    wire->requestFrom(addr, sizeof(bb_sensor_frame) + 2);
-    bb_sensor_frame frame_buffer;
-
-    uint8_t first_byte = wire->read();
-
-    if (first_byte == I2C_STATUS_EMPTY) {
-#ifdef I2C_DEBUG
-        Serial.print("[");
-        Serial.print(i);
-        Serial.print("]");
-        Serial.println("[I2C] No data available");
-#endif
-        return;
-    } else if (first_byte == I2C_STATUS_FINISHED) {
-        nodes[i].finished = 1;
-#ifdef I2C_DEBUG
-        Serial.print("[");
-        Serial.print(i);
-        Serial.print("]");
-        Serial.println("[I2C] Data finished");
-#endif
-        bool allFinished = true;
-        for (size_t n = 0; n < nodeCount; n++) {
-            if (!nodes[n].finished) {
-                allFinished = false;
-                break;
-            }
-        }
-
-        if (allFinished)
-            print_frames();
-
-        return;
-    } else if (first_byte == I2C_STATUS_DATA) {
-#ifdef I2C_DEBUG
-        Serial.print("[");
-        Serial.print(i);
-        Serial.print("]");
-        Serial.println("[I2C] Found frame");
-#endif
-        wire->readBytes((uint8_t *) &frame_buffer, sizeof(bb_sensor_frame));
-        if (seq != frame_buffer.seq) {
-#ifdef I2C_DEBUG
-            Serial.print("[");
-            Serial.print(i);
-            Serial.print("]");
-            Serial.println("[I2C] Incorrect seq");
-#endif
-            return;
-        }
-
-        crc.calculate((uint8_t *) &frame_buffer, sizeof(bb_sensor_frame));
-        int readCrc = wire->read();
-        if (crc.getCrc() != readCrc) {
-#ifdef I2C_DEBUG
-            Serial.print("[");
-            Serial.print(i);
-            Serial.print("]");
-            Serial.println("[I2C] Incorrect CRC");
-#endif
-            return;
-        }
-
-        int frame_idx = frames_length;
-        for (size_t index = 0; index < frames_length; index++) {
-            if (frames[index].sensor.sensor_addr == frame_buffer.sensor_addr) {
-                frame_idx = index;
-                break;
-            }
-        }
-        frames[frame_idx].node_addresses[frames[frame_idx].node_count] = nodes[i].address;
-        frames[frame_idx].node_count++;
-
-        memcpy(&(frames[frame_idx].sensor), &frame_buffer, sizeof(bb_sensor_frame));
-        frames_length = max(min(frame_idx + 1, MAX_REGS), frames_length);
-#ifdef I2C_DEBUG
-        Serial.print("[");
-        Serial.print(i);
-        Serial.print("]");
-        Serial.println("[I2C] Saved frame");
-#endif
-    } else {
-#ifdef I2C_DEBUG
-    Serial.print("[");
-    Serial.print(i);
-    Serial.print("]");
-    Serial.println("[I2C] Received unknown frame");
-#endif
-    }
-
-    // Czyszczenie nadmiarowych bajtów
-    while (wire->available())
-        wire->read();
-}
 
 void setup() {
     mymors.begin();
@@ -214,32 +24,10 @@ void setup() {
         digitalWrite(LED_BUILTIN, LOW);
         delay(500);
     }
-    Wire.begin();
-    Wire1.begin();
     Serial.println("W0 uruchomiony...");
     mymors.queue('s');
-    pinPeripheral(WIRE1_SDA, PIO_SERCOM);
-    pinPeripheral(WIRE1_SCL, PIO_SERCOM);
 }
 
 void loop() {
-    mymors.handle();
-
-    if (data_stopwatch.isTimeout()) {
-        for (int i = 0; i < nodeCount; i++) {
-            if (!nodes[i].finished) {
-                requestData(i);
-            }
-        }
-        data_stopwatch.reset();
-    }
-
-    if (request_stopwatch.isTimeout()) {
-        memset(frames, 0, MAX_REGS * sizeof(bb_node_frame));
-        print_frames();
-        discoverI2CDevices();
-        printed_frames = false;
-        mymors.queue('n', 1);
-        request_stopwatch.reset();
-    }
+    mymors.handle();    
 }
--- src/W1/main.cpp
+++ src/W1/main.cpp
@@ -1,195 +1,95 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include "Crc8.h"
-#include <BroadcastBus.h>
 #include "morslib.h"
+#include <SPI.h>
+#include <LoRa.h>
 
 #ifndef NODE_ADDR
 #define NODE_ADDR 0x02            // ID tego węzła master
 #endif
 
-#define DATA_PULL_FREQ 2000     // Częstotliwość odczytu (ms)
-#define MAX_SENSORS   8         // Maks. liczba urządzeń I2C
-#define MAX_DATA_SIZE 8         // Maks. liczba rejestów z jednego urządzenia
+#define DEST_ADDR 0x02            // Roboczo do usunięcia przez MJ
+
+struct PingPacket {
+  uint8_t from;
+  uint8_t to;
+  uint8_t type;
+};
+
+unsigned long lastPing = 0;
 
 morslib mymors(LED_BUILTIN, 200);
-
-int last_seq = -1;
-
-BroadcastBus bus = BroadcastBus();
-Stopwatch seqStopwatch = Stopwatch();
-
-bb_sensor_frame frame = {
-    .node_addr = NODE_ADDR,
-    .sensor_addr = 0,
-    .seq = 1,
-    .flags = 0b11010010,
-    .regs_len = 2,
-    .regs = {
-    {.addr = 0x11, .data = 41},
-    {.addr = 0x12, .data = 223},
-    {.addr = 0x1A, .data = 125},
-    {.addr = 0x1B, .data = 96},
-    {.addr = 0x1C, .data = 84},
-    {.addr = 0x2A, .data = 72},
-    {.addr = 0x2B, .data = 193},
-    {.addr = 0x2C, .data = 147},
-    },
-};
-
-// Struktury
-struct SensorInfo {
-  uint8_t address;
-};
-
-SensorInfo sensors[MAX_SENSORS];
-uint8_t sensorCount = 0;
-
-unsigned long lastRequestTime = 0;
-uint8_t seqNum = 0;
-
-uint8_t dataBuffer[MAX_SENSORS][MAX_DATA_SIZE*2];
-
-void clearDataBuffer() {
-    memset(dataBuffer, 0, sizeof(dataBuffer));
-}
-
-// Faza wykrywania urządzeń I2C
-void discoverI2CDevices() {
-  Serial.println("Skanowanie magistrali I2C...");
-  sensorCount = 0;
-
-  for (uint8_t addr = 1; addr < 127; addr++) {
-    if (addr == 0x60 || addr == 0x6B)
-      continue;
-    
-    Wire.beginTransmission(addr);
-    uint8_t error = Wire.endTransmission();
-
-    if (error == 0) {
-      if (sensorCount < MAX_SENSORS) {
-        sensors[sensorCount].address = addr;
-        Serial.println(addr, HEX);
-        sensorCount++;
-      }
-    }
-    delay(10);
-  }
-
-  Serial.print("Znaleziono ");
-  Serial.print(sensorCount);
-  Serial.println(" sensorów");  
-}
-
-// Odczyt danych z danego czujnika
-uint8_t readSensor(uint8_t addr, uint8_t i) {
-  clearDataBuffer();
-  Serial.print("odczyt po adresie ");
-  Serial.println(addr, HEX);
-  uint8_t N = 0;
-
-  // Zapytanie o rozmiarze 1 bajt
-  Wire.requestFrom(addr, (uint8_t)1);
-  frame.sensor_addr = addr;
-  if (Wire.available()) {
-    N = Wire.read();
-  }
-  else {
-    return 0;
-  }
-
-  //N *= 2;
-  Serial.print("N=");
-  Serial.println(N);
-  // Sprawdzenie poprawności rozmiaru
-  if (N == 0 || N*2 > MAX_DATA_SIZE) return 0;
-
-  frame.regs_len = N;
-
-  // Odczyt właściwych danych
-  int bytesRead = Wire.requestFrom(addr, N*2);
-  for (int j = 0; j < N; j++)
-  {
-    if ((j+1)*2 > bytesRead)
-      break;
-    
-    frame.regs[j].addr = Wire.read();
-    frame.regs[j].data = Wire.read();
-  }
-
-  seqStopwatch.reset();
-
-  bus.sendSensor(frame);
-
-  return bytesRead;
-}
-
-void acquireData(){
-  Serial.println("pobieranie danych");
-  discoverI2CDevices();
-  for (uint8_t i = 0; i < sensorCount; i++) {
-    uint8_t addr = sensors[i].address;
-    
-    uint8_t bytesRead = readSensor(addr, i);
-    Serial.print("Odczytano wiad o rozmiarze: ");
-    Serial.println(bytesRead);
-  }
-  bus.sendFinish(frame.seq, sensorCount);
-}
 
 void setup() {
   mymors.begin();
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);   
-  /*while(!Serial) {
+  while(!Serial) {
       digitalWrite(LED_BUILTIN, HIGH);
       delay(1000);
       digitalWrite(LED_BUILTIN, LOW);
       delay(500);
-  }*/
-  Wire.begin();
-  bus.begin();
+  }
   Serial.print("W");
   Serial.print(NODE_ADDR);
   Serial.println(" uruchomiony");
+  
+  if(!LoRa.begin(8681E5)) {
+    Serial.println("LoRa init - niepowodzenie");
+  }
+  Serial.println("LoRa init - udane");
+  
   mymors.queue('s');
+}
+
+void sendPing() {
+  PingPacket pkt = { NODE_ADDR, DEST_ADDR, 1 };
+  Serial.println("PING sending");
+  
+  LoRa.beginPacket();
+  LoRa.write((uint8_t*)&pkt, sizeof(pkt));
+  LoRa.endPacket();
+  
+  Serial.println("PING sent");
+}
+
+void sendPong(uint8_t dest) {
+  PingPacket pkt = { NODE_ADDR, dest, 2 };
+  Serial.println("PONG sending");
+
+  LoRa.beginPacket();
+  LoRa.write((uint8_t*)&pkt, sizeof(pkt));
+  LoRa.endPacket();
+
+  Serial.println("PONG sent");
 }
 
 void loop() {
   mymors.handle();
-  switch (bus.bSerial1.receiveCmd()) {
-    case BB_MASK_REQ:
-        if (bus.bSerial1.receiveData(1)) {
-          frame.seq = bus.bSerial1.rxBuffer.data[0];
-          if (frame.seq != last_seq){
-            acquireData();
-            mymors.queue('a');
-            mymors.queue('e');
-            last_seq = frame.seq;
-          }
-          bus.bSerial1.reset();
-        }
-        break;
-      }
-      switch (bus.bSerial2.receiveCmd()) {
-        case BB_MASK_REQ:
-        if (bus.bSerial2.receiveData(1)) {
-          frame.seq = bus.bSerial2.rxBuffer.data[0];
-          if (frame.seq != last_seq){
-            acquireData();
-            mymors.queue('a');
-            mymors.queue('i');
-            last_seq = frame.seq;
-          }
-          bus.bSerial2.reset();
-        }
-        break;
+  
+  if (millis() - lastPing > 2000) {
+    lastPing = millis();
+    sendPing();
+  }
+
+  // ---- RECEIVE ----
+  int packetSize = LoRa.parsePacket();
+  if (packetSize == sizeof(PingPacket)) {
+    PingPacket pkt;
+    LoRa.readBytes((uint8_t*)&pkt, sizeof(pkt));
+
+    if (pkt.to != NODE_ADDR) return;
+
+    if (pkt.type == 1) {
+      Serial.print("PING received from ");
+      Serial.println(pkt.from);
+      sendPong(pkt.from);
     }
+    else if (pkt.type == 2) {
+      Serial.print("PONG received from ");
+      Serial.println(pkt.from);
+    }
+  }
 
-  //if (millis() - lastRequestTime >= DATA_PULL_FREQ) {
-  //  
-  //  lastRequestTime = millis();
-  //}
-
-  delay(10);
+  //delay(10);
 }
