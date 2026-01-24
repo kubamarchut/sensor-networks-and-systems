b02d1fa430f47ccb76590d7fc10fb6c9f7e4299d
Kuba Marchut <kuba.marchut@gmail.com>
2026-01-24 17:48:22 +0100

    code for physical sensor

--- src/S1/main.cpp
+++ src/S1/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
 #include <WirelessCommunication.h>
 #include <Indicator.h>
+#include <TCS3200.h>
 
 #ifndef NODE_ADDR
 #define NODE_ADDR 0x07
+#endif
+
+//#define FAKE_SENSOR
+#define S0_PIN 4
+#define S1_PIN 3
+#define S2_PIN 2
+#define S3_PIN 1
+#define OUT_PIN 5
+
+#ifndef FAKE_SENSOR
+TCS3200 tcs3200(S0_PIN, S1_PIN, S2_PIN, S3_PIN, OUT_PIN);
 #endif
 
 Indicator indicator(9, 10, 11);
@@ -24,9 +36,23 @@ void updateRGBWithDelta(RGB &color, int maxDelta) {
   color.b = constrain(color.b + random(-maxDelta, maxDelta + 1), 0, 255);
 }
 
+#ifndef FAKE_SENSOR
+void performMeasurement(RGB &color) {
+  RGBColor rgb = tcs3200.read_rgb_color();
+  color.r = (uint8_t) constrain(rgb.red,   0, 255);
+  color.g = (uint8_t) constrain(rgb.green, 0, 255);
+  color.b = (uint8_t) constrain(rgb.blue,  0, 255);
+}
+#endif
+
 void readData() {
   delay(100);
+  #ifdef FAKE_SENSOR
   updateRGBWithDelta(currentColor, 5);
+  #endif
+  #ifndef FAKE_SENSOR
+  performMeasurement(currentColor);
+  #endif
 }
 
 void setup() {
@@ -41,13 +67,21 @@ void setup() {
   indicator.setColor(Indicator::OFF);
   
   Serial.begin(115200);
-  #ifdef LORA_DEBUG:
+  #ifdef LORA_DEBUG
     while (!Serial){
       digitalWrite(LED_BUILTIN, HIGH);
       delay(1000);
       digitalWrite(LED_BUILTIN, LOW);
       delay(500);
     }
+  #endif
+
+  #ifndef FAKE_SENSOR
+  tcs3200.begin();
+  tcs3200.frequency_scaling(TCS3200_OFREQ_2P);
+  tcs3200.calibrate_light(1367, 1993, 1723);
+  tcs3200.calibrate_dark(10269, 19202, 17411);
+  tcs3200.calibrate();
   #endif
 
   Serial.print("S");
